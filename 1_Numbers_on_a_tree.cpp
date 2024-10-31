241a7882dcd1ffc14ffb2e67bd22852141975126
sticlandaiana <sticlan.daiana.k9c@student.ucv.ro>
2024-10-31 18:15:48 +0200

    Add files via upload

---
+++ 1_Numbers_on_a_tree.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+//https://open.kattis.com/problems/numbertree
+using namespace std;
+//pe fiecare nivel se afal 2^nivel noduri
+//valoarea radacinii va fi 2^0+2^1+...+2^inaltime
+// deci 2^(inaltime+1)-1
+int main() {
+    int h;
+    string ruta;
+    cin >> h >> ruta;
+
+    // calculam valoarea radacinii
+    int radacina = pow(2, h + 1)-1;
+    
+    int scazator=1;
+    
+
+
+    // pt fiecare nod se va stabili scazatorul raportat la nivelul anterior
+    //iar valoarea scazatorului va creste de fiecare data
+    //si se va scade la final din radacina +1 pentru a se obtine valoarea nodului
+
+    for (char i : ruta) {
+        scazator = scazator * 2;
+        if (i == 'R')
+            scazator++;
+    }
+    
+    int nod = radacina - scazator + 1;
+    cout <<nod << endl;
+    return 0;
+}
